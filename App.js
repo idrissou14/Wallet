96cd77d8d02a82657082661769be47a4913c4b36
idrissou14 <131742935+idrissou14@users.noreply.github.com>
2024-10-24 08:35:53 +0100

    .

--- App.js
+++ App.js
@@ -1,12 +1,29 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+import  HomeScreen from './app/HomeScreen';
+import BudgetScreen from './app/BudgetScreen';
+
+const Tab = createMaterialTopTabNavigator();
+
+// const MyTabs = () =>{
+//   return(
+//       <Tab.Navigator>
+//         <Tab.Screen name="Home" component={HomeScreen} />
+//         <Tab.Screen name="Budgets" component={BudgetScreen} />
+//       </Tab.Navigator>
+//   );
+// }
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+     <NavigationContainer>
+        <Tab.Navigator>
+          <Tab.Screen name="Comptes" component={HomeScreen} />
+          <Tab.Screen name="Budgets" component={BudgetScreen} />
+        </Tab.Navigator>
+     </NavigationContainer>
   );
 }
 
